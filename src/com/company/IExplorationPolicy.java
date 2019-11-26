4602925ccff4ccc3f6c1be3d6c2f3a0ae616d8b7
nguyentuthanhdn <44814087+nguyentuthanhdn@users.noreply.github.com>
2019-11-25 20:52:34 -0700

    qlearning

---
+++ src/com/company/IExplorationPolicy.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public interface IExplorationPolicy {
+    /**
+     * The method chooses an action depending on the provided estimates. the
+     * estimates can be any sort of estimate, which values usefulness of the action
+     * (expected summary reward, discounted reward, etc).
+     *
+     * @param actionEstimates Action estimates.
+     * @return Returns selected action.
+     */
+    int ChooseAction(double[] actionEstimates);
+}
---
+++ src/com/company/QLearningTest.java
@@ -0,0 +1,129 @@
+package com.company;
+
+import java.util.Random;
+
+//TODO Refer to this resource for creating and finishing this class
+// https://github.com/accord-net/java/blob/master/Catalano.MachineLearning/src/Catalano/MachineLearning/QLearning.java#L70
+public class QLearningTest {
+    // amount of possible states
+    private int states;
+    // amount of possible actions
+    private int actions;
+    // q-values
+    private double[][] qValues;
+    // exploration policy
+    private IExplorationPolicy explorationPolicy;
+    // discount factor
+    /**
+     * Discount factor for the expected summary reward. The value serves as
+     * multiplier for the expected reward. So if the value is set to 1,
+     * then the expected summary reward is not discounted. If the value is getting
+     * smaller, then smaller amount of the expected reward is used for actions'
+     * estimates update.
+     *
+     * @param discountFactor
+     */
+    private double discountFactor = 0.95;
+    // learning rate
+    //     * Learning rate, [0, 1].
+    //     * The value determines the amount of updates Q-function receives
+    //     * during learning. The greater the value, the more updates the function receives.
+    //     * The lower the value, the less updates it receives.
+    //     *
+    private double learningRate = 0.25;
+
+    /**
+     * Initializes a new instance of the QLearning class.
+     *
+     * @param states            Amount of possible states.
+     * @param actions           Amount of possible actions.
+     * @param explorationPolicy Exploration policy.
+     * @param randomize         Randomize action estimates or not.
+     */
+    public QLearningTest(int states, int actions, IExplorationPolicy explorationPolicy, boolean randomize) {
+        this.states = states;
+        this.actions = actions;
+        this.explorationPolicy = explorationPolicy;
+        // create Q-array
+        qValues = new double[states][];
+        for (int i = 0; i < states; i++) {
+            qValues[i] = new double[actions];
+        }
+
+        if (randomize) {
+            Random r = new Random();
+            for (int i = 0; i < states; i++) {
+                for (int j = 0; j < actions; j++) {
+                    qValues[i][j] = r.nextDouble() / 10;
+                }
+            }
+        }
+    }
+
+    public int getStates() {
+        return states;
+    }
+
+    public int getActions() {
+        return actions;
+    }
+
+    public IExplorationPolicy getExplorationPolicy() {
+        return explorationPolicy;
+    }
+
+    public void setExplorationPolicy(IExplorationPolicy explorationPolicy) {
+        this.explorationPolicy = explorationPolicy;
+    }
+
+    public double getLearningRate() {
+        return learningRate;
+    }
+
+    public void setLearningRate(double learningRate) {
+        this.learningRate = Math.max(0.0, Math.min(1.0, learningRate));
+    }
+
+    public double getDiscountFactor() {
+        return discountFactor;
+    }
+
+    public void setDiscountFactor(double discountFactor) {
+        this.discountFactor = Math.max(0.0, Math.min(1.0, discountFactor));
+    }
+
+    /**
+     * Get next action from the specified state.
+     *
+     * @param state Current state to get an action for.
+     * @return Returns the action for the state.
+     */
+    public int GetAction(int state) {
+        return explorationPolicy.ChooseAction(qValues[state]);
+    }
+
+    /**
+     * Update Q-function's value for the previous state-action pair.
+     *
+     * @param previousState Previous state.
+     * @param action        Action, which leads from previous to the next state.
+     * @param reward        Reward value, received by taking specified action from previous state.
+     * @param nextState     Next state.
+     */
+    public void UpdateState(int previousState, int action, double reward, int nextState) {
+        // next state's action estimations
+        double[] nextActionEstimations = qValues[nextState];
+        // find maximum expected summary reward from the next state
+        double maxNextExpectedReward = nextActionEstimations[0];
+
+        for (int i = 1; i < actions; i++) {
+            if (nextActionEstimations[i] > maxNextExpectedReward)
+                maxNextExpectedReward = nextActionEstimations[i];
+        }
+        // previous state's action estimations
+        double[] previousActionEstimations = qValues[previousState];
+        // update expexted summary reward of the previous state
+        previousActionEstimations[action] *= (1.0 - learningRate);
+        previousActionEstimations[action] += (learningRate * (reward + discountFactor * maxNextExpectedReward));
+    }
+}
